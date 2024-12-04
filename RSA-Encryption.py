9be927f299630988a26571bfa86acdfbfbc4836d
jarrenseson <jarrenys@hawaii.edu>
2024-12-04 08:28:46 -1000

    Update RSA-Encryption.py

--- RSA-Encryption.py
+++ RSA-Encryption.py
@@ -2,11 +2,25 @@ import random
 import math
 
 # Checks if number is prime
-def is_prime(n):
-    if n<= 1:
+def miller_rabin(n, k=5):
+    if n == 2 or n == 3:
+        return True
+    if n == 1 or n % 2 == 0:
         return False
-    for i in range(2,math.sqrt(n)):
-        if n%i==0:
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
             return False
     return True
 
@@ -27,17 +41,16 @@ def extended_euclidean_function(e,phi):
 def modulo_inverse(e,phi):
     g, x, y = extended_euclidean_function(e,phi)
     if g!=1:
-        print("no modulo inverse")
-        return
+        raise ValueError("no modulo inverse")
     return x%phi
 
 def RSA(message):
     p=0
     q=0
     while True:
-        p=random.randint(10**100, 10**1000)
-        q=random.randint(10**100, 10**1000)
-        if(is_prime(p) and is_prime(q)):
+        p=random.randint(10**100, 10**200)
+        q=random.randint(10**100, 10**200)
+        if(miller_rabin(p) and miller_rabin(q)):
             break
 
     n=p*q
@@ -49,17 +62,19 @@ def RSA(message):
 
     private=(e,n)
     public=(d,n)
+    int_message=int(''.join([str(ord(char)).zfill(3) for char in message]))
 
-    encrypted_message=pow(int(message),e,n)
+    encrypted_message=pow(int_message, e, n)
     print(f"encrypted message: {encrypted_message}")
 
-    decrypted_message=pow(int(encrypted_message),d,n)
+    decrypted_int_message=pow(int(encrypted_message),d,n)
+    decrypted_message=''.join([chr(int(str(decrypted_int_message)[i:i+3])) for i in range(0,len(str(decrypted_int_message)), 3)])
     print(f"decrypted message: {decrypted_message}")
 
     return encrypted_message, decrypted_message
 
 def main():
-    print(RSA("hello there"))
+    RSA("hello there")
 
 if __name__ =="__main__":
     main()
